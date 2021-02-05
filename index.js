84e03f54869f3186cb31984deb534c3a5b1e5b5d
ceddlyburge <ceddlyburge@gmail.com>
2021-02-05 16:58:48 +0000

    Try (but fail) to get adal to authenticate with blob api

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// index.js
 const { InteractiveBrowserCredential } = require("@azure/identity");
 const { BlobServiceClient } = require("@azure/storage-blob");
 const { PublicClientApplication } = require("@azure/msal-browser");
+// import AuthenticationContext from 'adal-angular/lib/adal'
+// import { AuthenticationContext } from 'react-adal'
 // Now do something interesting with BlobServiceClient
 
 const listContainersButton = document.getElementById("list-containers-button");
 const listContainersMsalButton = document.getElementById("list-containers-msal-button");
+const listContainersAdalButton = document.getElementById("list-containers-adal-button");
 const status = document.getElementById("status");
 
 const reportStatus = message => {
@@ -13,10 +15,11 @@ const reportStatus = message => {
     status.scrollTop = status.scrollHeight;
 }
 
+// -----------------------------------
+
 const listContainers = async () => {
     // this works with the cedd-test-2021-02-04 app registration
     // It gets its own token under the hood, I think using implicit flow, using either adal or msal v1.x I guess
-    const account = "archiebackup";
     const defaultAzureCredential = new InteractiveBrowserCredential(
         {
             clientId:"611fa3fe-d569-4b94-b9ba-31128f9087bd",
@@ -27,7 +30,7 @@ const listContainers = async () => {
     );
 
     const blobServiceClient = new BlobServiceClient(
-    `https://${account}.blob.core.windows.net`,
+    `https://archiebackup.blob.core.windows.net`,
     defaultAzureCredential
     );
 
@@ -38,10 +41,12 @@ const listContainers = async () => {
         reportStatus(`Container: ${containerItem.value.name}`);
         containerItem = await iter.next();
     }
-    reportStatus(`Done.`);
+    reportStatus(`Done internally by blob client library.`);
 }
 
 listContainersButton.addEventListener("click", listContainers);
+
+// -----------------------------------
 
 const listContainersMsal = async () => {
     // MSAL.js 2.x improves on MSAL.js 1.x by supporting the authorization code flow in the browser instead of the implicit grant flow.
@@ -87,30 +92,62 @@ const listContainersMsal = async () => {
         reportStatus(`Container: ${containerItem.value.name}`);
         containerItem = await iter.next();
     }
-    reportStatus(`Done.`);
+    reportStatus(`Done msal.`);
 }
 
 listContainersMsalButton.addEventListener("click", listContainersMsal);
 
-class CustomTokenCredential {
-    token;
-    expiresOn;
-  
-    constructor(token, expiresOn) {
-      this.token = token;
-      if (expiresOn == undefined) {
-        this.expiresOn = Date.now() + 60 * 60 * 1000;
-      } else {
-        this.expiresOn = expiresOn.getTime();
-      }
-    }
-  
-    async getToken(_scopes, _options) {
-      console.log(_scopes, _options);
-      return {
-        token: this.token,
-        expiresOnTimestamp: this.expiresOn,
-      };
+
+// -----------------------------------
+
+export const tenant = '90610670-1a80-434a-88c3-e568bce39bc5'
+// export const turbineApiClientId = '4e7e6e03-790b-4d40-ad3b-6b704f6ebbec'
+// export const layoutApiClientId = '0edc92fe-5663-4dd7-a94a-9b3f831916dc'
+// export const projectApiClientId = '32426fce-8689-48db-81cd-b9535220215c'
+// export const yieldApiClientId = '113519de-6613-40f6-8412-c5e53d541d7f'
+export const ramClientId = '7189c18a-5c52-4a52-afc4-25bf36fa2175'
+export const clientId = '57b1c265-bba9-4096-af18-0471c694b089'
+
+
+
+  export const adalConfig = {
+    tenant: tenant,
+    clientId: clientId, // this should be the client id of the ram
+    redirectUri: window.location.origin,
+    callback: listContainersAdalFromToken,
+    endpoints: {
+      // [turbinesEndpoint.API]: turbineApiClientId,
+      // [projectsEndpoint.API]: projectApiClientId,
+      // [energyYieldsEndpoint.API]: yieldApiClientId,
+      // [layoutsEndpoint.API]: layoutApiClientId
     }
   }
+  
+  let authContext  = new AuthenticationContext(adalConfig)
+  
+  // Check For & Handle Redirect From AAD After Login
+  //var isCallback = authContext.isCallback(window.location.hash);
+  //authContext.handleWindowCallback();
+  //$errorMessage.html(authContext.getLoginError());
+  
+  // if (isCallback && !authContext.getLoginError()) {
+  //     window.location = authContext._getItem(authContext.CONSTANTS.STORAGE.LOGIN_REQUEST);
+  // }
+  
+  const listContainersAdal = async () => {
+    // todo: The App Registration needs to be set up differently for this to work
+    // todo: this works with the cedd-test-2021-02-03 app registration
+    authContext.acquireTokenRedirect(
+        clientId, // this should be the client id of the blob storage api I think, not of the ram any more
+        null,
+        null
+      )
+
+    // authContext.acquireToken(
+    //     clientId, // this should be the client id of the blob storage api I think, not of the ram any more
+    //     listContainersAdalFromToken
+    //   )
+}
+
+//listContainersAdalButton.addEventListener("click", listContainersAdal);
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Blob api authentication examples
+
+Code mostly comes from https://docs.microsoft.com/en-us/azure/storage/blobs/quickstart-blobs-javascript-browser
+
+`parcel index.html` to serve page
+
+There is another example in the `ms-identity-javascript-v2` folder, that I couldn't get to read from blob storage, but it could get the details of the current user, and read emails and things like that. It is from a microsoft example repo, and has its own readme.
+
+I got the blob api working with msal v1 and msal v2, but not with adal (there is another repo with the adal example).
